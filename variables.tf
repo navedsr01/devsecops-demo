6635dcb0e9292464ff601c8b9a0a7e9c8996b94b
Naved Rafiq <navedsr@yahoo.com>
2025-02-07 21:13:44 +0000

    Creating AWS Infra - Demo-v1

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "bucket_name" {
   type    = string
+  default = "github-actions-bucket-12345"
 }
