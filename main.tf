25f79e85d21bb9074e5ab41c32d835e2e3baab8c
Naved Rafiq <navedsr@yahoo.com>
2025-02-07 20:49:05 +0000

    Creating AWS Infra - Demo-v1

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "github_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "upload_file" {
+  bucket = aws_s3_bucket.github_bucket.id
+  key    = "test-file.txt"
+  source = "test-file.txt"  # Local file to upload
+  etag   = filemd5("test-file.txt")
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
